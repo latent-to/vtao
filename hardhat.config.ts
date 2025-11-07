04bc5fd57cc41de66554abe32f04b9afbaddad7c
Cameron Fairchild <c@latent.to>
2025-11-07 15:53:13 -0500

    add msic files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,4 @@
+import "@nomicfoundation/hardhat-foundry";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-verify";
